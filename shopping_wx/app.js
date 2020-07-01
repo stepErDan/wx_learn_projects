a7089777eeef656fd5fa63dd5aa4b4bd0510d710
dingx <dx402967115@qq.com>
2020-07-01 22:57:27 +0800

    迈出了坚实的一步：获取用户基本信息授权

--- shopping_wx/app.js
+++ shopping_wx/app.js
@@ -1,24 +1,12 @@
 //app.js
 var config = require("./config.js");
-var login = require("./utils/login.js");
 
 App({
   host : config.host,
   onLaunch: function () {
-    //先进行授权
-    
-    // 获取token
-    var token = wx.getStorage('token');
-    //判断token是否为空，若token为空，进行登录
-    if(token == ''){
-      login.login;
-    }
-
-    // 获取用户信息
-    var userInfo = wx.getStorageSync('userInfo');
 
   },
   globalData: {
     userInfo: null
-  }
+  },
 })
