a902df41bb9452a88245b98d9a5c1b17480704d8
dingx <dx402967115@qq.com>
2020-07-13 22:28:55 +0800

    轮播加模版

--- shopping_wx/pages/index/index.js
+++ shopping_wx/pages/index/index.js
@@ -7,6 +7,16 @@ Page({
   data: {
     show: false,
     value:"",
+    swiperCon: {
+      imgUrls: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+      indicatorDots: true,//是否显示面板指示点
+      indicatorColor: "rgba(0, 0, 0, .3)",//指示点颜色
+      indicatorActiveColor: "#007aff",//当前选中的指示点颜色
+      autoplay: true,//是否自动切换
+      interval: 5000,//自动切换时间间隔
+      duration: 1000,//滑动动画时长
+      circular: true,//是否采用衔接滑动
+    }
   },
   onLoad: function () {
     wx.getSetting({
